9c0563acd2eaeb6ac4e3f266faa23a23b893918a
KeithCheongWaiKeong <keithcheongwaikeong@gmail.com>
2021-07-23 00:25:14 +0800

    Changed placeholder pages

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -14,14 +14,10 @@ const Contact = () => {
 const useStyles = makeStyles(theme => ({
 	mainContainer: {
 		background: theme.palette.primary.dark,
-		position: 'absolute',
-		top: '50%',
-		left: '50%',
-		transform: 'translate(-50%,-50%)',
 		width: '100%',
-		maxHeight: window.innerHeight,
-		textAlign: 'center',
-		zIndex: 1,
+		minHeight: window.innerHeight,
+		alignItems: 'center',
+		justifyContent: 'center',
 	},
 	text: {
 		textAlign: 'center',
